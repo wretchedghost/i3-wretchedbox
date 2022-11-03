acdcb4f469be236ca964821dfd94ad3101b53405
wretchedghost <wretchedghost0@gmail.com>
2022-11-03 12:21:47 -0500

    changes

--- dotfile-link.sh
+++ dotfile-link.sh
@@ -15,11 +15,14 @@ dir=~/github/i3-wretchedbox
 # list of files/folders to symlink in homedir
 files=".fehbg .tmux.conf .xinitrc .Xresources"
 
-mv ~/.config/i3 $dot && ln -s $dir/.config/i3 ~/.config/
-mv ~/.config/rofi $dot && ln -s $dir/.config/rofi ~/.config/
-ln -s $dir/.config/dusnt ~/.config/
-ln -s $dir/Pictures/Portugal ~/Pictures/Portugal
-mv ~/.local/share/applications/libreoffice-writer.desktop $dot && ln -s $dir/libreoffice-writer.desktop ~/.local/share/applications/
+mv ~/.config/i3 $dot && ln -s $dir/.config/i3 ~/.config/ && echo "Creating symlink in home directory."
+mv ~/.config/rofi $dot && ln -s $dir/.config/rofi ~/.config/ && echo "Creating symlink in home directory."
+rm -rf ~/.config/dunst && ln -s $dir/.config/dusnt ~/.config/ && echo "Creating symlink in home directory."
+rm -rf ~/Pictures/Portugal && ln -s $dir/Pictures/Portugal ~/Pictures/ && echo "Creating symlink in home directory."
+rm -rf ~/Pictures/example && ln -s $dir/Pictures/example ~/Pictures/ && echo "Creating symlink in home directory."
+rm -rf ~/.screenlayout && ln -s $dir/.screenlayout ~/ && echo "Creating symlink in home directory."
+mv ~/.local/share/applications/libreoffice-writer.desktop $dot && ln -s $dir/libreoffice-writer.desktop ~/.local/share/applications/ && echo "Creating symlink in home directory."
+
 
 ##########
 
@@ -30,6 +33,7 @@ mv ~/.local/share/applications/libreoffice-writer.desktop $dot && ln -s $dir/lib
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
+    cd ~/
     mv $file $dot
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
@@ -37,13 +41,8 @@ done
 
 # Checks if file is symbolic and is also not broken by testing if it is linked to a file or directory.
 if [ ! -L ~/.fehbg ]; then
-    if [ -e ~/.fehbg ]; then
     echo "Something messed up. Keeping all files stored in $dot"
-    else 
+else 
     echo "Everything is now symlinked. Erasing $dot which is no longer needed"
-    fi
-elif [ -e ~/.fehbg ]; then
-    echo "File is not a Link."
-else
-    echo "File is missing."
+    rm -rf $dot
 fi
